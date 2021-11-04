d714b30d37fa55a732e3747dc0cb5c9423b4c1ad
Chinmay Savadikar <savadikarc@gmail.com>
2021-11-04 15:06:31 -0400

    bug fix

--- data_utils/preprocessing.py
+++ data_utils/preprocessing.py
@@ -79,6 +79,6 @@ class Preprocessor:
             seg = np.zeros(seg_img.shape[:2])
             seg[seg_img[:, : 0] == 255] = 1 # R
             seg[seg_img[:, : 2] == 255] = 2 # B
-            bounding_boxes = self.generate_bounding_boxes(seg, seg_values=seg_values)
+            bounding_boxes = self.generate_bounding_boxes([seg], seg_values=seg_values)
             filename = image.split(".")[0]
             self.save_bounding_boxes(base_save_dir, filename, bounding_boxes)
