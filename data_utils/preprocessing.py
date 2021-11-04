45634b766f16aeffa8c80acb8907b6c2871bb090
Chinmay Savadikar <savadikarc@gmail.com>
2021-11-03 20:18:56 -0400

    wrapper

--- data_utils/preprocessing.py
+++ data_utils/preprocessing.py
@@ -1,6 +1,9 @@
 import os
 import nibabel as nib
+import numpy as np
 import cv2
+
+from extraction import extract_and_save_slices
 
 
 SEGMENTATION_FILE = "segmentation.nii.gz"
@@ -10,10 +13,11 @@ class Preprocessor:
 
     def __init__(self, dir):
         self.dir = dir
-        self.case_ids = os.listdir(dir)
+        self.case_ids = [f for f in os.listdir(dir) if "." not in f]
 
-    def extract_and_save_slices(self, vol, seg):
-        pass
+    def extract_and_save_slices(self, cid, vol, seg, destination):
+        filenames, indices = extract_and_save_slices(cid, vol, seg, destination)
+        return filenames, indices
 
     def generate_bounding_boxes(self, segmentations):
         
@@ -44,7 +48,7 @@ class Preprocessor:
 
     def load_segmentation_volume(self, case_path):
         segmentation_path = os.path.join(case_path, SEGMENTATION_FILE)
-        seg = nib.load(segmentation_path).get_data()
+        seg = nib.load(segmentation_path).get_data().astype(np.int32)
         return seg
     
     def load_case(self, case_id):
@@ -54,8 +58,15 @@ class Preprocessor:
         return image_volume, segmentation_volume
 
     def run(self, base_save_dir):
+        files = []
         for case_id in self.case_ids:
             vol, seg = self.load_case(case_id)
-            filenames, segmentations = self.extract_and_save_slices(vol, seg)
+            filenames, indices = self.extract_and_save_slices(case_id, vol, seg, base_save_dir)
+            segmentations = [seg[i] for i in indices]
             bounding_boxes = self.generate_bounding_boxes(segmentations)
             self.save_bounding_boxes(base_save_dir, case_id, bounding_boxes)
+            files += filenames
+
+        file_list = "\n".join(files)
+        with open(os.path.join(base_save_dir, "filenames.txt"), "w") as f:
+            f.write(file_list)
